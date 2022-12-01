b571f2b7a7a0454bf2f709b6b94155536baeab74
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-30 23:16:13 -0500

    commit tree 1b666dac83cc29d07ecc8a15b1fc6824c0ef7422

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,91 @@
-# your code goes here
+class Person 
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize (name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def name
+    @name
+  end
+
+  def bank_account=(value)
+    @bank_account = value
+  end
+
+  def happiness=(value)
+    @happiness = if value > 10
+      10
+    elsif value < 0
+      0
+    else
+      value
+    end
+  end
+
+  def hygiene=(value)
+    @hygiene = if value > 10
+      10
+    elsif value < 0
+      0
+    else
+      value
+    end
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    @friend = Person.new(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      friend.happiness -= 2
+      self.happiness -=2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      friend.happiness += 1
+      self.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end 
+  end
+end
+
+# Get paid/receive payments
+# Take a bath
+# Call a friend
+# Start a conversation
+# State if they are happy and/or clean
